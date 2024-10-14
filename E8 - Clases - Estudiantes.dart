22c8edf2332b79fc7b4f6012b88ca030cac3a740
Adrian <adrian.estrada99@outlook.com>
2024-10-14 12:19:34 -0600

    Actualizado

--- E8 - Clases - Estudiantes.dart
+++ E8 - Clases - Estudiantes.dart
@@ -12,6 +12,10 @@ class Estudiantes{
 }
 
 void main(){
-  Estudiantes estudiantes = new Estudiantes("Adrián", 24, "a22010585", 9.75);
-  estudiantes.MostrarInfo();
+  Estudiantes estudiante1 = new Estudiantes("Adrián", 24, "a22010585", 9.75);
+  Estudiantes estudiante2 = new Estudiantes("Ángel", 24, "a22010148", 8.2);
+  Estudiantes estudiante3 = new Estudiantes("Mauricio", 24, "a22114454", 8.5);
+  estudiante1.MostrarInfo();
+  estudiante2.MostrarInfo();
+  estudiante3.MostrarInfo();
 }
