379870be563583728bc6914e367fdfae864fde4a
Adrian <adrian.estrada99@outlook.com>
2024-10-14 12:50:57 -0600

    Ejercicio de Clase

---
+++ E9 - Clases - Banco.dart
@@ -0,0 +1,32 @@
+class CuentaBancaria{
+  String Titular;
+  double Saldo;
+
+  CuentaBancaria(this.Titular, this.Saldo);
+
+  void Depositar(){
+    Saldo += 50;
+    print(" > El nuevo saldo es: $Saldo");
+  }
+  void Retirar(){
+    if(Saldo == 0){
+      print(" > No cuenta con saldo suficiente.");
+    }
+    else{
+      Saldo -= 50;
+      print(" > Retiró 50 pesos. Su nuevo saldo es $Saldo.");
+    }
+  }
+  void Consultar(){
+    print(" > Hola $Titular, tu saldo es de $Saldo");
+  }
+}
+
+void main(){
+  CuentaBancaria cuenta = new CuentaBancaria("Adrián", 155);
+
+  cuenta.Depositar();
+  cuenta.Depositar();
+  cuenta.Retirar();
+  cuenta.Consultar();
+}
